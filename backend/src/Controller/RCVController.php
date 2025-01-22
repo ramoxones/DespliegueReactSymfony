2373b437f43f52e262bcc1dc7ff04abc4c67e886
ramoxones <ramoncarrascovelazquez@gmail.com>
2025-01-22 21:46:51 +0100

    proyecto completo tu nombre completo

--- backend/src/Controller/RCVController.php
+++ backend/src/Controller/RCVController.php
@@ -4,7 +4,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 class RCVController
 {
-#[Route('/api/ini', name: 'api_ini')]
+#[Route('/api/RCV', name: 'api_RCV')]
 public function index(): Response
 {
 return new Response('Symfony de Ramon Carrasco Responde a la llamada de
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from 'react';
 function App() {
  const [message, setMessage] = useState('');
  useEffect(() => {
- fetch('/api/ini')
+ fetch('/api/RCV')
  .then((response) => response.text())
  .then((data) => setMessage(data));
  }, []);
  return (
  <div>
- <h1>Frontend en React de Nombre Completo</h1>
+ <h1>Frontend en React de Ramon Carrasco Velazquez</h1>
  <p>Esta aplicación se conecta al backend de Symfony pidiéndole una respuesta</p>
  <p>Backend response: {message}</p>
  </div>
